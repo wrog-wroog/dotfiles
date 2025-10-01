11d9ce84da58ca18ff89e85e8eb31306ef55e31a
kotpakiet <wrog@wroog.com>
2025-10-01 15:11:57 +0200

    Lazy VIM

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
-
-vim.wo.relativenumber = true
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
